63579b15487160e21b4002a64a77223408629762
kvakirsanov <anton.kirsanov@gmail.com>
2024-12-12 23:30:32 +0600

    Customize messages

--- display-mtproto-data.js
+++ display-mtproto-data.js
@@ -5,41 +5,49 @@ console.log("[+] Base address:", baseAddress);
 
 // --------------------------------------------------------------------------------------------------
 var aesIgeDecryptRaw_addr = baseAddress.add("0x678c620"); // 0x678c620 - aesIgeDecryptRaw addr in 5.9.0
+var CRYPTO_ctr128_encrypt_addr = baseAddress.add("0x678c850"); // 0x678c850 - CRYPTO_ctr128_encrypt addr in 5.9.0
+
 console.log("[+] aesIgeDecryptRaw address:", aesIgeDecryptRaw_addr);
+console.log("[+] CRYPTO_ctr128_encrypt_addr address:", CRYPTO_ctr128_encrypt_addr);
+
+var maxBytes = 1024 * 32;
+
+function preview(data, len)
+{
+   return hexdump(data, { length: len });
+}
+
+// --------------------------------------------------------------------------------------------------
 Interceptor.attach(aesIgeDecryptRaw_addr, {
-
   onEnter: function (args) {
-    console.log("[+] " + aesIgeDecryptRaw_addr + " called");
-    this.a0 = args[0];
-    this.a1 = args[1];
+//    console.log("[+] " + aesIgeDecryptRaw_addr + " called");
+    this.buf = args[1];
     this.numBytes = args[2].toInt32();
-
   },
-
   onLeave: function (result) {
-    console.log("RECV: " + hexdump(this.a1, { length: this.numBytes, ansi: true }));
-    console.log("[+] " + aesIgeDecryptRaw_addr + " returned:", result.toInt32());
+    if(this.numBytes > maxBytes)
+      console.log("RECV <=        " + preview(this.buf, this.maxBytes).trim() + " [STRIPPED " + (this.numBytes - maxBytes) + " bytes]");
+    else
+      console.log("RECV <=        " + preview(this.buf, this.numBytes).trim());
+
+//    console.log("[+] " + aesIgeDecryptRaw_addr + " returned:", result.toInt32());
   }
 });
 
 // --------------------------------------------------------------------------------------------------
-var CRYPTO_ctr128_encrypt_addr = baseAddress.add("0x678c850"); // 0x678c850 - CRYPTO_ctr128_encrypt addr in 5.9.0
-console.log("[+] CRYPTO_ctr128_encrypt_addr address:", CRYPTO_ctr128_encrypt_addr);
-
 Interceptor.attach(CRYPTO_ctr128_encrypt_addr, {
-
   onEnter: function (args) {
+//    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " called");
+    this.buf = args[0];
+    this.numBytes = args[2].toInt32();
 
-    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " called");
-
-    this.a0 = args[0];
-    var numBytes = args[2].toInt32();
-
-    console.log("SEND: " + hexdump(this.a0, { length: numBytes, ansi: true }));
+    if(this.numBytes > maxBytes)
+      console.log("SEND =>        " + preview(this.buf, this.maxBytes).trim() + " [STRIPPED " + (this.numBytes - maxBytes) + " bytes]");
+    else
+      console.log("SEND =>        " + preview(this.buf, this.numBytes).trim());
   },
-
   onLeave: function (result) {
-    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " returned:", result.toInt32());
+//    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " returned:", result.toInt32());
   }
 });
 /**/
