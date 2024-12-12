f2e13104bf08a8ff4aff254dacabddd75e141c82
kvakirsanov <anton.kirsanov@gmail.com>
2024-12-12 10:00:05 +0600

    added aesIgeDecryptRaw hook

--- display-mtproto-data.js
+++ display-mtproto-data.js
@@ -1,19 +1,36 @@
-
 var moduleName = "";
-var offset = "0x678c850"; // CRYPTO_ctr128_encrypt addr in 5.9.0
 
 var baseAddress = Module.findBaseAddress(moduleName);
-
 console.log("[+] Base address:", baseAddress);
 
-var functionAddress = baseAddress.add(offset);
-console.log("[+] Function address:", functionAddress);
+// --------------------------------------------------------------------------------------------------
+var aesIgeDecryptRaw_addr = baseAddress.add("0x678c620"); // 0x678c620 - aesIgeDecryptRaw addr in 5.9.0
+console.log("[+] aesIgeDecryptRaw address:", aesIgeDecryptRaw_addr);
+Interceptor.attach(aesIgeDecryptRaw_addr, {
 
-Interceptor.attach(functionAddress, {
+  onEnter: function (args) {
+    console.log("[+] " + aesIgeDecryptRaw_addr + " called");
+    this.a0 = args[0];
+    this.a1 = args[1];
+    this.numBytes = args[2].toInt32();
+
+  },
+
+  onLeave: function (result) {
+    console.log(hexdump(this.a1, { length: this.numBytes, ansi: true }));
+    console.log("[+] " + aesIgeDecryptRaw_addr + " returned:", result.toInt32());
+  }
+});
+
+// --------------------------------------------------------------------------------------------------
+var CRYPTO_ctr128_encrypt_addr = baseAddress.add("0x678c850"); // 0x678c850 - CRYPTO_ctr128_encrypt addr in 5.9.0
+console.log("[+] CRYPTO_ctr128_encrypt_addr address:", CRYPTO_ctr128_encrypt_addr);
+
+Interceptor.attach(CRYPTO_ctr128_encrypt_addr, {
 
   onEnter: function (args) {
 
-    console.log("[+] " + functionAddress + " called");
+    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " called");
 
     this.a0 = args[0];
     var numBytes = args[2].toInt32();
@@ -22,6 +39,7 @@ Interceptor.attach(functionAddress, {
   },
 
   onLeave: function (result) {
-    console.log("[+] " + functionAddress + " returned:", result.toInt32());
+    console.log("[+] " + CRYPTO_ctr128_encrypt_addr + " returned:", result.toInt32());
   }
 });
+/**/
